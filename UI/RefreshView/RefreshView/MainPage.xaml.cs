8d7b2b31272141921e7a836b61e7e84c86f2dfeb
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-01-16 23:49:54 -0600

    feature: RefreshView

---
+++ UI/RefreshView/RefreshView/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using ReactiveUI;
+using ReactiveUI.XamForms;
+using Xamarin.Forms;
+
+namespace RefreshViewDemo
+{
+    public partial class MainPage : ReactiveContentPage<MainPageViewModel>
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.BindCommand(ViewModel, x => x.RefreshCommand, x => x.RefreshView);
+            this.OneWayBind(ViewModel, x => x.IsRefreshing, x => x.RefreshView.IsRefreshing);
+        }
+    }
+}
---
+++ UI/RefreshView/RefreshView/MainPageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Reactive;
+using System.Reactive.Linq;
+using Xamarin.Forms;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace RefreshViewDemo
+{
+    public class MainPageViewModel : ReactiveObject
+    {
+        const int RefreshDuration = 2;
+        int itemNumber = 1;
+        readonly Random random;
+        private readonly ObservableAsPropertyHelper<bool> _isRefreshing;
+
+        public MainPageViewModel()
+        {
+            random = new Random();
+            Items = new ObservableCollection<Item>();
+            AddItems();
+
+            RefreshCommand = ReactiveCommand.CreateFromTask(async () => await RefreshItemsAsync(), outputScheduler: RxApp.MainThreadScheduler);
+
+            _isRefreshing =
+                this.WhenAnyObservable(x => x.RefreshCommand.IsExecuting)
+                    .StartWith(false)
+                    .DistinctUntilChanged()
+                    .ToProperty(this, nameof(IsRefreshing), scheduler: RxApp.MainThreadScheduler);
+        }
+
+        public bool IsRefreshing => _isRefreshing.Value;
+
+        public ObservableCollection<Item> Items { get; private set; }
+
+        public ReactiveCommand<Unit, Unit> RefreshCommand { get; set; }
+
+        void AddItems()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Items.Add(new Item
+                {
+                    Color = Color.FromRgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255)),
+                    Name = $"Item {itemNumber++}"
+                });
+            }
+        }
+
+        async Task RefreshItemsAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(RefreshDuration));
+            AddItems();
+        }
+    }
+}
